471d5264ad54dd98a1e21c7e39f243d9b1c24824
proxe <trentin.shaun.frederick@gmail.com>
2018-09-21 13:04:47 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 
 # blender info
 bl_info = {
-    'name': 'Name Stack Public Edition',
+    'name': 'Name Stack Public',
     'author': 'proxe',
     'version': (0, '8          Commit: 488'),
     'blender': (2, 78, 0),
